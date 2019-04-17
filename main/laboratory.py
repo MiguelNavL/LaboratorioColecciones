4647ffec664f2fc92af919e48616954e2f8d7704
Miguel Navas <miguel.navas.2019@upb.edu.co>
2019-04-17 14:33:05 -0500

    Avances literales d,e,f,g

--- main/laboratory.py
+++ main/laboratory.py
@@ -2,6 +2,8 @@
 Solución del laboratorio
 """
 
+import statistics
+
 datosSantander = {
     "Enero": 22,
     "Febrero": 27,
@@ -92,3 +94,71 @@ print("el mes mas caliente de LA GUAJIRA fue", mesCalienteGuajira[0], "con", mes
 
 mesCalienteNarino = mes_mas_caliente(datosNarino)
 print("el mes mas caliente de NARIÑO fue", mesCalienteNarino[0], "con", mesCalienteNarino[1], "grados")
+
+
+# Los ultimos 2 meses mas calientes
+
+def promedio_tres_meses_mas_calientes(datos):
+    orden = sorted(datos.values())
+    return (orden[9] + orden[10] + orden[11]) / 3
+
+
+promMesesSantander = promedio_tres_meses_mas_calientes(datosSantander)
+print("el promedio de los tres mes mas caliente de SANTANDER fue", promMesesSantander)
+
+promMesesNarino = promedio_tres_meses_mas_calientes(datosNarino)
+print("el promedio de los tres mes mas caliente de NARIÑO fue", promMesesNarino)
+
+promMesesGuajira = promedio_tres_meses_mas_calientes(datosGuajira)
+print("el promedio de los tres mes mas caliente de GUAJIRA fue", promMesesGuajira)
+
+ordenPromedio = sorted([promMesesSantander, promMesesNarino, promMesesGuajira])
+print("el promedio mayor es", ordenPromedio[2])
+
+"""
+conglomerado = {
+    33: {
+        "mes": "Noviembre",
+        "region": "Santander"
+    },
+    35: {
+        "mes": "Abril",
+        "region": "Guajira"
+    },
+    29: {
+        "mes": "Diciembre",
+        "region": "Nariño"
+    }
+}
+"""
+
+conglomerado = {
+    mesCalienteSantander[1]: {
+        "mes": mesCalienteSantander[0],
+        "region": "Santander"
+    },
+    mesCalienteGuajira[1]: {
+        "mes": mesCalienteGuajira[0],
+        "region": "Guajira"
+    },
+    mesCalienteNarino[1]: {
+        "mes": mesCalienteNarino[0],
+        "region": "Nariño"
+    }
+}
+
+tempMasCaliente = sorted(conglomerado)[2]
+print("la temperatura mas caliente del año es", tempMasCaliente)
+
+datosMasCaliente = conglomerado.get(tempMasCaliente)
+print("el mes mas caliente del año es", datosMasCaliente.get("mes"))
+print("la region mas caliente del año es", datosMasCaliente.get("region"))
+
+desviacionEstandarSantander = statistics.stdev(datosSantander.values())
+print("la desviasión estandar de la región de SANTANDER es", desviacionEstandarSantander)
+
+desviacionEstandarGuajira = statistics.stdev(datosGuajira.values())
+print("la desviasión estandar de la región de GUAJIRA es", desviacionEstandarGuajira)
+
+desviacionEstandarNarino = statistics.stdev(datosNarino.values())
+print("la desviasión estandar de la región de NARIÑO es", desviacionEstandarNarino)
